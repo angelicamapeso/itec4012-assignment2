0e682cdf0b95bb9ac5be471af81713b060052270
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-08 21:23:25 -0500

    fix htmlFor error

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -6,7 +6,7 @@ import { FiCamera, FiSearch } from "react-icons/fi";
 export default function Search({ className }) {
   return (
     <form id="search-form" className={className}>
-      <label for="search">Search</label>
+      <label htmlFor="search">Search</label>
       <div id="search-input-wrapper">
         <input id="search" placeholder="What are you looking for?"/>
         <FiSearch className="icon" aria-hidden="true"/>
