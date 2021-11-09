028b1e21b8528b4a4f3f5e80353dc027e75b181b
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-08 23:47:08 -0500

    add left hamburger menu

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -9,27 +9,29 @@ import { FiTruck, FiHeart, FiMenu } from "react-icons/fi";
 export default function MainNav() {
   return (
     <header>
-      <div className="header-wrapper">
-        <a className="logo-wrapper" href="/"><img className="logo" alt="Home" src={Logo} /></a>
-        <ul className="nav-text">
-          <li><a href="/">Products</a></li>
-          <li><a href="/">Rooms</a></li>
-          <li><a href="/">Offers</a></li>
-          <li><a href="/">Green Friday</a></li>
-        </ul>
-        <ul className="nav-icons">
-          <li><Button><MdOutlineLocationOn className="icon" /></Button></li>
-          <li id="truck"><Button><FiTruck className="icon" /></Button></li>
-          <li><Button><MdPersonOutline className="icon" /></Button></li>
-          <li><Button><FiHeart className="icon" /></Button></li>
-          <li><Button><MdOutlineShoppingBasket className="icon" /></Button></li>
-          <li id="menu"><Button><FiMenu className="icon" /></Button></li>
-        </ul>
-        <Search className="search-form" />
-      </div>
-      <div className="hamburger-menu-icon">
-
-      </div>
+      <nav>
+        <div className="header-wrapper">
+          <a className="logo-wrapper" href="/"><img className="logo" alt="Home" src={Logo} /></a>
+          <ul className="nav-text">
+            <li><a href="/" className="bold">Products</a></li>
+            <li><a href="/" className="bold">Rooms</a></li>
+            <li><a href="/" className="bold">Offers</a></li>
+            <li><a href="/" className="bold">Green Friday</a></li>
+          </ul>
+          <ul className="nav-icons">
+            <li><Button><MdOutlineLocationOn className="icon" /></Button></li>
+            <li id="truck"><Button><FiTruck className="icon" /></Button></li>
+            <li><Button><MdPersonOutline className="icon" /></Button></li>
+            <li><Button><FiHeart className="icon" /></Button></li>
+            <li><Button><MdOutlineShoppingBasket className="icon" /></Button></li>
+            <li id="menu"><Button><FiMenu className="icon" /></Button></li>
+          </ul>
+          <Search className="search-form" />
+        </div>
+        <div className="hamburger-menu-icon">
+          <Button><FiMenu className="icon" /></Button>
+        </div>
+      </nav>
     </header>
   );
 }
