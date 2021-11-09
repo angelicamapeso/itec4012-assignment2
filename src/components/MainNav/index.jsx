c698426aa47b78be0f5731d94d86aa0d88ccefa7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-08 23:23:55 -0500

    fix styling, add text links

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -11,13 +11,19 @@ export default function MainNav() {
     <header>
       <div className="header-wrapper">
         <a className="logo-wrapper" href="/"><img className="logo" alt="Home" src={Logo} /></a>
+        <ul className="nav-text">
+          <li><a href="/">Products</a></li>
+          <li><a href="/">Rooms</a></li>
+          <li><a href="/">Offers</a></li>
+          <li><a href="/">Green Friday</a></li>
+        </ul>
         <ul className="nav-icons">
           <li><Button><MdOutlineLocationOn className="icon" /></Button></li>
           <li id="truck"><Button><FiTruck className="icon" /></Button></li>
           <li><Button><MdPersonOutline className="icon" /></Button></li>
           <li><Button><FiHeart className="icon" /></Button></li>
           <li><Button><MdOutlineShoppingBasket className="icon" /></Button></li>
-          <li><Button><FiMenu className="icon" /></Button></li>
+          <li id="menu"><Button><FiMenu className="icon" /></Button></li>
         </ul>
         <Search className="search-form" />
       </div>
