a9ddffd2ddd02bb46bd3f1d0b3f4bc5f9ead6dc4
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-09 19:20:46 -0500

    display promotional page section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
 import "./styles.scss";
 
+import Promotional from "../../page-sections/Promotional";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Promotional />
+    </>
   );
 }
