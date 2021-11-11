12dbbf672f69e7df684c12e46b2c58d3687f3a40
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-11 00:16:06 -0500

    add first carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,36 @@
 import "./styles.scss";
+import { CAROUSEL1 } from "./carousel_items.js";
 
+// icons
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+// components
 import Promotional from "../../page-sections/Promotional";
+import Card from "../../components/Card";
+import Button from "../../components/Button";
+import LinkWrapper from "../../components/LinkWrapper";
 
 export default function Home() {
+  console.log(CAROUSEL1);
+
   return (
     <>
       <Promotional />
+      <div id="carousel1">
+        <div className="carousel-wrapper">
+          { CAROUSEL1.map(card => (
+              <LinkWrapper>
+                <Card src={card.src} color={card.color}>
+                  <div className="card-text">
+                    <a href="/"><h3>{card.title}</h3></a>
+                    <p>{card.para}</p>
+                  </div>
+                  <Button><HiOutlineArrowRight className="arrow-icon"/></Button>
+                </Card>
+              </LinkWrapper>
+            )) }
+        </div>
+      </div>
     </>
   );
 }
