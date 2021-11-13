ef751bd8feddad619351dbed049acfff402eb6ec
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-13 16:27:20 -0500

    add carousel1 to homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,28 +9,25 @@ import Promotional from "../../page-sections/Promotional";
 import Card from "../../components/Card";
 import Button from "../../components/Button";
 import LinkWrapper from "../../components/LinkWrapper";
+import Carousel from "../../components/Carousel";
 
 export default function Home() {
-  console.log(CAROUSEL1);
-
   return (
     <>
       <Promotional />
-      <div id="carousel1">
-        <div className="carousel-wrapper">
+      <Carousel id="carousel1" numPages={{ def: 6, sm: 3, md: 2, lg: 2 }} >
           { CAROUSEL1.map(card => (
               <LinkWrapper>
                 <Card src={card.src} color={card.color}>
                   <div className="card-text">
-                    <a href="/"><h3>{card.title}</h3></a>
+                    <h3 className="underline">{card.title}</h3>
                     <p>{card.para}</p>
                   </div>
                   <Button><HiOutlineArrowRight className="arrow-icon"/></Button>
                 </Card>
               </LinkWrapper>
             )) }
-        </div>
-      </div>
+      </Carousel>
     </>
   );
 }
