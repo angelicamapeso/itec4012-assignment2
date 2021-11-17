be584d6acb1ec5fb853a96ae5e125e73f4c64df1
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-16 20:55:26 -0500

    add second image list data

---
+++ src/pages/Home/image_lists.jsx
@@ -0,0 +1,81 @@
+// images
+import imageList1_1 from "../../img/imageList1_1.webp";
+import imageList1_2 from "../../img/imageList1_2.webp";
+import imageList1_3 from "../../img/imageList1_3.webp";
+import imageList1_4 from "../../img/imageList1_4.webp";
+import imageList1_5 from "../../img/imageList1_5.webp";
+import imageList1_6 from "../../img/imageList1_6.webp";
+
+import imageList2_1 from "../../img/imageList2_1.webp";
+import imageList2_2 from "../../img/imageList2_2.webp";
+import imageList2_3 from "../../img/imageList2_3.jpg";
+
+export const IMAGE_LIST_1 = [
+  {
+    src: imageList1_1,
+    txt: "Duvets",
+  },
+  {
+    src: imageList1_2,
+    txt: "Candle holders & candles",
+  },
+  {
+    src: imageList1_3,
+    txt: "Blinds",
+  },
+  {
+    src: imageList1_4,
+    txt: "Cookware & tableware",
+  },
+  {
+    src: imageList1_5,
+    txt: "Bathroom",
+  },
+  {
+    src: imageList1_6,
+    txt: "Cushions & cushions covers",
+  },
+];
+
+export const IMAGE_LIST_2 = [
+  {
+    src: imageList2_1,
+    title: "IKEA Family",
+    descrip: <>
+      <p>Love your IKEA products longer</p>
+      <p>
+        Join us in this free workshop to receive some tips and tricks on how to extend the life of your IKEA products so that you can love them for longer!
+      </p>
+    </>,
+    btnTxt: "Register for the Event",
+  },
+  {
+    src: imageList2_2,
+    title: "IKEA for Business",
+    descrip: <>
+      <p>
+        Hospitality Restaurant &amp; café. Are you looking for practical and modern restaurant and café furniture for your dining establishment? Create a space that is as warm and friendly as it is functional and easy to maintain.
+      </p>
+      <p>
+        <a href="/">Create an IKEA for Business account</a> and we'll help bring your business ideas to life. You'll be able to setup profiles for your colleagues, track your purchases and receive personalized inspiration relevant to your industry.
+      </p>
+    </>,
+    btnTxt: "Learn more about IKEA for Business",
+  },
+  {
+    src: imageList2_3,
+    title: "IKEA Food",
+    descrip: <>
+      <p>
+      The plant ball has the taste, texture and juicy bite of a meatball – but it’s made with pea protein, oats, potatoes, onion and apple. Available through <a href="/">IKEA Food Click and collect</a>.
+      </p>
+      <p>
+        <strong>HUVUDROLL</strong>, Plant balls, frozen 1 lb 2oz (500g).<br />
+        <strong>New lower price $5.99 each</strong>. Regular price: $6.99 each.
+      </p>
+    </>,
+    btnTxt: "Shop IKEA Food Click & Collect",
+    btnTxtSec: "Learn more about IKEA Food",
+  },
+];
+
