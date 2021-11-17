dbd1e90d8977f426a048be9f812577c1e739ed03
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-16 19:25:38 -0500

    add array for first image list

---
+++ src/pages/Home/image_lists.js
@@ -0,0 +1,34 @@
+import imageList1_1 from "../../img/imageList1_1.webp";
+import imageList1_2 from "../../img/imageList1_2.webp";
+import imageList1_3 from "../../img/imageList1_3.webp";
+import imageList1_4 from "../../img/imageList1_4.webp";
+import imageList1_5 from "../../img/imageList1_5.webp";
+import imageList1_6 from "../../img/imageList1_6.webp";
+
+export const IMAGE_LIST_1 = [
+  {
+    src: imageList1_1,
+    txt: "Duvets",
+  },
+  {
+    src: imageList1_2,
+    txt: "Candle holders & candles",
+  },
+  {
+    src: imageList1_3,
+    txt: "Blinds",
+  },
+  {
+    src: imageList1_4,
+    txt: "Cookware & tableware",
+  },
+  {
+    src: imageList1_5,
+    txt: "Bathroom",
+  },
+  {
+    src: imageList1_6,
+    txt: "Cushions & cushions covers",
+  },
+];
+
