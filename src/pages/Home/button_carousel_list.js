c0f87bbf18a34ed0acb8c901e1a0817064823065
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-17 18:09:02 -0500

    add list for inspiration button list

---
+++ src/pages/Home/button_carousel_list.js
@@ -0,0 +1,12 @@
+export const BUTTON_LIST_1 = [
+  "Bedroom",
+  "Living room",
+  "Kitchen",
+  "Home office",
+  "Outdoor",
+  "Bathroom",
+  "Baby & children's room",
+  "Dining",
+  "Hallway",
+  "Accessories",
+];
