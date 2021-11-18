b467a1cbaf5748cdb895af2573d0b8c2395f80c4
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-18 16:40:23 -0500

    add cart recommended products list

---
+++ src/pages/Cart/product_lists.js
@@ -0,0 +1,161 @@
+// originals
+import prodList1_1a from "../../img/prodList1_1a.webp";
+import prodList1_2a from "../../img/prodList1_2a.webp";
+import prodList1_3a from "../../img/prodList1_3a.webp";
+import prodList1_4a from "../../img/prodList1_4a.webp";
+import prodList1_5a from "../../img/prodList1_5a.webp";
+import prodList1_6a from "../../img/prodList1_6a.webp";
+import prodList1_7a from "../../img/prodList1_7a.webp";
+import prodList1_8a from "../../img/prodList1_8a.webp";
+import prodList1_9a from "../../img/prodList1_9a.webp";
+import prodList1_10a from "../../img/prodList1_10a.webp";
+import prodList1_11a from "../../img/prodList1_11a.webp";
+import prodList1_12a from "../../img/prodList1_12a.webp";
+
+// alternatives
+import prodList1_1b from "../../img/prodList1_1b.webp";
+import prodList1_2b from "../../img/prodList1_2b.webp";
+import prodList1_3b from "../../img/prodList1_3b.webp";
+import prodList1_4b from "../../img/prodList1_4b.webp";
+import prodList1_5b from "../../img/prodList1_5b.webp";
+import prodList1_6b from "../../img/prodList1_6b.webp";
+import prodList1_7b from "../../img/prodList1_7b.webp";
+import prodList1_8b from "../../img/prodList1_8b.webp";
+import prodList1_9b from "../../img/prodList1_9b.webp";
+import prodList1_10b from "../../img/prodList1_10b.webp";
+import prodList1_11b from "../../img/prodList1_11b.webp";
+import prodList1_12b from "../../img/prodList1_12b.webp";
+
+export const PROD_LIST_1 = [
+  {
+    newProd: false,
+    src: prodList1_1a,
+    altSrc: prodList1_1b,
+    name: "BESTÅ / LACK",
+    descrip: 'TV storage combination, 94 1/2x16 1/2x50 3/4 " (240x42x129 cm)',
+    price: "474.98",
+    availMoreOptions: true,
+    showStarRating: false,
+  },
+  {
+    newProd: false,
+    src: prodList1_2a,
+    altSrc: prodList1_2b,
+    name: "SÄLLSKAPLIG",
+    descrip: 'Carafe with stopper, 34 oz (1 l)',
+    price: '9.99',
+    availMoreOptions: false,
+    showStarRating: true,
+    starRating: 4,
+    numReview: 1,
+  },
+  {
+    newProd: false,
+    src: prodList1_3a,
+    altSrc: prodList1_3b,
+    name: "GLADELIG",
+    descrip: 'Plate, 8x5 " (20x13 cm)',
+    price: "6.99",
+    perText: '2 pack',
+    availMoreOptions: false,
+    showStarRating: true,
+    starRating: 5,
+    numReview: 3,
+  },
+  {
+    newProd: true,
+    src: prodList1_4a,
+    altSrc: prodList1_4b,
+    name: "UPPSPEL",
+    descrip: 'Gaming desk, 55 1/8x31 1/2 " (140x80 cm)',
+    price: "679.00",
+    availMoreOptions: false,
+    showStarRating: false,
+  },
+  {
+    newProd: false,
+    src: prodList1_5a,
+    altSrc: prodList1_5b,
+    name: "FINNALA",
+    descrip: 'Sofa',
+    price: "1,189.00",
+    availMoreOptions: true,
+    showStarRating: false,
+  },
+  {
+    newProd: true,
+    src: prodList1_6a,
+    altSrc: prodList1_6b,
+    name: "TROTTEN",
+    descrip: 'Desk sit/stand, 47 1/4x27 1/2 " (120x70 cm)',
+    price: "239.00",
+    availMoreOptions: false,
+    showStarRating: false,
+  },
+  {
+    newProd: true,
+    src: prodList1_7a,
+    altSrc: prodList1_7b,
+    name: "MOLNART",
+    descrip: 'LED bulb E26 100 lumen, 7 " (180 mm)',
+    price: "24.99",
+    perText: "pack",
+    availMoreOptions: false,
+    showStarRating: false,
+  },
+  {
+    newProd: true,
+    src: prodList1_8a,
+    altSrc: prodList1_8b,
+    name: "TÅRTBAK",
+    descrip: 'Baking mold',
+    price: "9.99",
+    availMoreOptions: true,
+    showStarRating: false,
+  },
+  {
+    newProd: false,
+    src: prodList1_9a,
+    altSrc: prodList1_9b,
+    name: "GLADELIG",
+    descrip: 'Bowl, 4 ½ " (11 cm)',
+    price: "9.99",
+    perText: '4 pack',
+    availMoreOptions: true,
+    showStarRating: true,
+    starRating: 5,
+    numReview: 1,
+  },
+  {
+    newProd: true,
+    src: prodList1_10a,
+    altSrc: prodList1_10b,
+    name: "FULLÖSA",
+    descrip: 'Armchair',
+    price: "279.00",
+    availMoreOptions: false,
+    showStarRating: false,
+  },
+  {
+    newProd: false,
+    src: prodList1_11a,
+    altSrc: prodList1_11b,
+    name: "VARDAGEN",
+    descrip: 'Serving plate, 17x11 " (43x28 cm)',
+    price: "14.99",
+    availMoreOptions: false,
+    showStarRating: true,
+    starRating: 5,
+    numReview: 2,
+  },
+  {
+    newProd: true,
+    src: prodList1_12a,
+    altSrc: prodList1_12b,
+    name: "KÄTTIL",
+    descrip: 'Chair',
+    price: "49.99",
+    availMoreOptions: false,
+    showStarRating: false,
+  },
+];
