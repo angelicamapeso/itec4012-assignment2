9aaacad3d7ecf57fed9c9d0e9e02e08b2ad90104
angelicamapeso <angelica.mapeso@gmail.com>
2022-03-15 02:00:04 +0000

    Deploying to gh-pages from  @ 0308633bb39529dcbb87783076f783fec9e97b9a 🚀

--- src/components/DotButton/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.scss";
-
-export default function DotButton({
-  style = {},
-}) {
-  return (
-    <button style={style} className="dot-button"></button>
-  );
-}
--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const BREAKPOINTS = {
-  lg: '75em',
-  md: '56.25em',
-  sm: '37.5em',
-}
